c9ef33b9ccea034ce40da12e4a18faf483b2d296
Ethan Huang <ethanferminhuang@gmail.com>
2022-05-21 20:39:12 -0700

    Add files via upload
    Added PieList and BakeryInventory

--- CRUD_test/CakeList_script1.sql
+++ CRUD_test/CakeList_script1.sql
@@ -3,21 +3,55 @@ CREATE DATABASE IF NOT EXISTS Bakery;
 USE Bakery;
 
 CREATE TABLE CakeList (
-  `id` varchar(45) NOT NULL,
+  `cake_id` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
   `description` varchar(90) NOT NULL,
-  `price` int NOT NULL,
-  `stock` int NOT NULL,
-  primary key(`id`)
+  primary key(`cake_id`)
 );
 
-DROP TABLE CakeList;
+INSERT INTO 
+	CakeList (cake_id, name, description)
+VALUES 
+	('CID-00001', 'German Chocolate Cake', 'layered chocolate cake filled and topped with a coconut-pecan frosting'),
+	('CID-00002', 'Neapolitan Cake', 'cake with vanilla, chocolate and strawberry layer'),
+    ('CID-00003', 'Boston Cream Pie', 'sponge cake with cream and chocolate frosting');
 
-INSERT INTO CakeList (id, name, description, price, stock)
-VALUES ('ID-00001', 'German Chocolate Cake', 'layered chocolate cake filled and topped with a coconut-pecan frosting', 30, 12);
+SELECT * FROM CakeList;
 
-INSERT INTO CakeList (id, name, description, price, stock)
-VALUES ('ID-00022', 'Neapolitan Cake', 'cake with vanilla, chocolate and strawberry layer', 35, 7);
+CREATE TABLE PieList (
+	`pie_id` varchar(45) NOT NULL,
+    `name` varchar(45) NOT NULL,
+	`primary_ingredient` varchar(90) NOT NULL,
+    primary key(`pie_id`)
+);
 
-SELECT * FROM CakeList
 
+INSERT INTO 
+	PieList (pie_id, name, primary_ingredient)
+VALUES 
+	('PID-00001', 'Apple Pie', 'Apples'),
+	('PID-00002', 'Rhubarb Pie', 'Rhubarb'),
+	('PID-00003', 'Key Lime Pie', 'Limes'),
+    ('PID-00004', 'Banana Cream Pie', 'Banana');
+
+SELECT * FROM PieList;
+
+CREATE TABLE BakeryInventory (
+	`name` varchar(45) NOT NULL,
+    `id` varchar(45) NOT NULL,
+    `price` double NOT NULL,
+    `stock` int NOT NULL
+);
+
+INSERT INTO 
+	BakeryInventory (name, id, price, stock)
+VALUES 
+	('Apple Pie', 'PID-00001', 24.99, 12),
+    ('Rhubarb Pie', 'PID-00002', 28.99, 8),
+    ('Key Lime Pie', 'PID-00003', 19.99, 10),
+    ('Banana Cream Pie', 'PID-00004', 20.99, 9),
+    ('German Chocolate Cake', 'CID-00001', 29.99, 8),
+    ('Neapolitan Cake', 'CID-00002', 32.99, 15),
+    ('Boston Cream Pie', 'CID-00003', 25.99, 22);
+	
+SELECT * FROM BakeryInventory;
