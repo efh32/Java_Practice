513ec9faee0cf17853d026d1251a89fd03aaafbe
Ethan Huang <ethanferminhuang@gmail.com>
2022-05-25 20:16:55 -0700

    Add files via upload

---
+++ REST/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity handleRuntimeException() {
+        return new ResponseEntity("something happened", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
